c6edc82ee7d00dae6afe0318bef5ebf9b101df33
Ryan <rpaulos@clark.edu>
2018-11-01 16:39:48 -0700

    Request needs better tests?

--- unittesting.py
+++ unittesting.py
@@ -1,6 +1,7 @@
 import unittest
 import os
 from request import Request
+from usermanager import UserManager
 import old
 
 from councilconnect import CouncilConnect
@@ -12,13 +13,14 @@ headers = {'Content-Type': 'application/json', 'Authorization': "Bearer " + CCto
 
 class TestCouncilConnect(unittest.TestCase):
 
-    #def setUp(self):
+    def test_alert(self):
+        CouncilConnect.alert_recipients = ['rpaulos@clark.edu']
+        CouncilConnect.alert('UnitTesting')
 
     def test_alert_recipients_added(self):
         CouncilConnect.alert_recipient('rpaulos@clark.edu')
-        CouncilConnect.alert_recipient('jrobertson@clark.edu')
-        rep_list = ['rpaulos@clark.edu', 'jrobertson@clark.edu']
-        self.assertEqual(rep_list, CouncilConnect.alert_recipients)
+        rep_list = 'rpaulos@clark.edu'
+        self.assertEqual(rep_list, CouncilConnect.alert_recipient)
 
     def test_url_method(self):
         CouncilConnect.url('penguins')
@@ -30,15 +32,21 @@ class TestCouncilConnect(unittest.TestCase):
         self.assertEqual(
             CouncilConnect.council_headers, {'Content-Type': 'application/json', 'Authorization': "Bearer " + 'cheese'})
 
+    def test_account_method(self):
+        CouncilConnect.account(5)
+        self.assertEqual(CouncilConnect.canvas_account, 5)
+
     def tearDown(self):
         CouncilConnect.alert_recipients = []
         CouncilConnect.base_url = None
         CouncilConnect.cc_token = None
+        CouncilConnect.canvas_account = 1
 
 
 class TestRequest(unittest.TestCase):
 
-    def test_request_against_retrieve_users(self):
+    # Not really a great test
+    def test_Request_against_retrieve_users(self):
         payload = {
             'sort': 'username',
             'order': 'asc',
@@ -50,6 +58,23 @@ class TestRequest(unittest.TestCase):
         self.assertEqual(users, ret)
 
 
+class TestUserManager(unittest.TestCase):
+
+    def setUp(self):
+        CouncilConnect.url('https://councils.clark.edu/')
+        CouncilConnect.token(os.getenv('CCtoken'))
+
+    def test_retrieve_users_against_original_functions(self):
+        user_manager_data = UserManager.retrieve_users()
+        ret_data = old.retrieve_all_user_objects(1)
+        self.assertEqual(user_manager_data, ret_data)
+
+    def test_retrieve_user_info_vs_original(self):
+        user_manager_data = UserManager.retrieve_user_info(12)
+        ret_data = old.retrieve_user_info(12)
+        self.assertEqual(user_manager_data, ret_data)
+
+
 
 if __name__ == "__main__":
-    unittest.main()
+    unittest.TestSuite()
