5ecc2b19cda7d42e174d294184ef6bb029b9af95
Ryan <rpaulos@clark.edu>
2018-11-01 16:39:03 -0700

    basics implemented

--- councilconnect.py
+++ councilconnect.py
@@ -1,4 +1,6 @@
 import smtplib
+import os
+import time
 """
     Hope: this class will be a superclass for the top-level objects interacting with Council Connect. This will provide
     the subclasses the inherent ability to do things like send alert emails when something fails, etc.
@@ -9,23 +11,24 @@ class CouncilConnect(object):
     base_url = None
     cc_token = None
     council_headers = None
-    alert_recipients = []  # List of email addresses
+    recipient = None
+    canvas_account = 1
 
     @classmethod
     def alert(cls, message):
-        s = smtplib.SMTP(host='smtpout.clark.edu')
+        s = smtplib.SMTP(host='smtp.gmail.com', port=587)
         s.starttls()
-        try:
-            for recipient in cls.alert_recipients:
-                s.sendmail('CouncilConnectTools@clark.ed', recipient, message)
-        except Exception as e:
-            print(
-                "Error:{}  while attempting to send an alert email about "
-                "another error with message:{}".format(e, message))
+        email_password = os.getenv('CouncilConnectTools')
+        s.login("CouncilConnectTools@gmail.com", email_password)
+        s.sendmail('CouncilConnectTools@gmail.com', cls.recipient, message)
+
+    @classmethod
+    def account(cls, account):
+        CouncilConnect.canvas_account = account
 
     @classmethod
     def alert_recipient(cls, email):
-        cls.alert_recipients.append(email)
+        cls.alert_recipient = email
 
     @classmethod
     def url(cls, url):
