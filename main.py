4f897ee67753a5bb368e395cbdedac77b47f5ac3
Ryan <rpaulos@clark.edu>
2018-11-05 13:09:04 -0800

    Just random debug stuff

--- main.py
+++ main.py
@@ -7,11 +7,12 @@ import usermanager
 
 if __name__ == "__main__":
 
-    CouncilConnect.alert_recipient('RPaulos@clark.edu')
-    #CouncilConnect.alert_recipient('ryan.eliopoulos@clark.edu')
-    CouncilConnect.url('https://councils.clark.edu/')
-    CouncilConnect.token(os.getenv('CCtoken'))
+    #CouncilConnect.url('https://councils.clark.edu/')  # The trailing slash is required
+    #CouncilConnect.token(os.getenv('CCtoken'))  # Will require explanation in the docs
+    CouncilConnect.alert_recipient('RPaulos@clark.edu')  # where failure alerts will be sent
+    CouncilConnect.init_session()  # Gets things ready. MUST be called.
 
-    CouncilConnect.alert('ohmygoodness')
+    #user = UserManager.search_person_id('@!!$testosteroni')
     #users = UserManager.retrieve_users()
 
+    UserManager.search_person_id('@!!$testosteroni')
