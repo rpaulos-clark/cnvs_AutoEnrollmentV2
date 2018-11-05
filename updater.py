5860a46a7bf76f4d4b967d1babe940cbdcc72eb2
Ryan <rpaulos@clark.edu>
2018-11-05 13:08:46 -0800

    Purged all

--- updater.py
+++ updater.py
@@ -1,14 +1,1 @@
 
-
-class Updater(object):
-
-    def __init__(self):
-        self.alert_recipients = []
-        self.server = None  # e.g. prometheus
-        self.database = None  # e.g. ResearchAnalytics
-        self.url = None  # e.g. councils.clark.edu
-        self.courses = []
-
-    def council_course(self, course_number, default_role_id, default_enrollment_type):
-        ...
-
